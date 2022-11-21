6532906ed7088196c2090fd434ae1e9615aef244
abbygoyal <abhishekgoyal3669@gmail.com>
2022-11-21 18:53:42 +0530

    first commit

---
+++ src/pages/homepage/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../../Components/header/Header";
+import Listing from "../../Components/listing/Listing";
+import Sidebar from "../../Components/sidebar/Sidebar";
+import "./Home.css";
+
+function home() {
+  return (
+    <div>
+      <Header />
+      <div className="home-page">
+        <Listing />
+        <Sidebar />
+      </div>
+    </div>
+  );
+}
+
+export default home;
